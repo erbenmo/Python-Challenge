2aa216e4e3465210ab7af4768be100a0961f8a38
Erben Mo <erbenmo@gmail.com>
2012-08-28 15:35:29 -0400

    challenge 1 finished

---
+++ 1/translate.py
@@ -0,0 +1,18 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+encrypted = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+url = "map"
+
+clear = ""
+
+for c in url:
+    if c in alphabet:
+        index = alphabet.index(c)
+        index += 2
+        index %= len(alphabet)
+        clear += alphabet[index]
+    else:
+        clear += c
+
+print clear
